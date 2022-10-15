e2da6d2e166791602acc2aefb4d13c664757e782
dportillo <daniloportillo200030@gmail.com>
2022-10-14 22:57:08 -0600

    router outlet added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FavoritePageComponent } from './modules/favorites/pages/favorite-page/favorite-page.component';
 
 @NgModule({
   declarations: [ //TODO: Declaraciones , componentes, directivas , pipes
-    AppComponent
+    AppComponent, 
   ],
   imports: [ // Todo: solo se importan modules
     BrowserModule,
