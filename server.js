bd21faf984b3ee836045b73e67b81e04d7de5ab4
dportillo <daniloportillo200030@gmail.com>
2022-10-28 18:15:28 -0600

    fix server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname+'/dist/spotify'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-spotify/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/spotify/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
